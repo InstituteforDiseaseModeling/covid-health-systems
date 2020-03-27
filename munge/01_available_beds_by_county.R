740465bdb0007c1ffd76f85f851e89926b491dcb
Lauren George <lgeorge@idmod.org>
2020-03-27 14:18:32 -0700

    rename some more files

--- munge/01_available_beds_by_county.R
+++ munge/01_available_beds_by_county.R
@@ -9,7 +9,7 @@ library(RColorBrewer)
 library(viridis)
 library(here)
 
-DS <- data.frame(read_xlsx(here("data/Hospital capacity and population by county 2018.xlsx")))
+DS <- data.frame(read_xlsx(here("data/hospital_capacity_and_population_by_county_2018.xlsx")))
 DS <- subset(DS,AAC_per10k > 0 & ICU_per100k > 0)
 DS <- subset(DS,AAC_per10k < 100 & ICU_per100k < 100)
 DS$State <- as.factor(DS$State)
@@ -21,7 +21,7 @@ p2 <- ggplot(data=DS,aes(y=ICU_per100k,x=State))+geom_boxplot()+theme_bw() +
   xlab("")+ylab("Number of Beds") + labs(title="Available ICU Beds per 100k Population")+
   theme(axis.text.x = element_text(angle=90,vjust=.5,hjust=1))
 
-pdf(paste("../plots/Available Beds per Population Data.pdf",sep=""),width=10,height=5)
+pdf(here("reports/Available Beds per Population Data.pdf")),width=10,height=5)
 plot(p1)
 plot(p2)
 dev.off()
